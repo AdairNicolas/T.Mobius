3b92bbbb617538775d8ceb356299d72d8440fd6a
AdairNicolas <adairnicolas1@gmail.com>
2022-03-09 21:12:58 -0600

    cambios

---
+++ TransformacionDeMobius.py
@@ -0,0 +1,9 @@
+
+print("Bienvenido al programa sobre las transformacxiones de Mobius\n")
+
+z1=input("Ingresa el primer numero complejo: ")
+z2=input("Ingresa el segundo numero complejo: ")
+z3=input("Ingresa el tercer numero complejo: ")
+z4=input("Ingresa el cuarto numero complejo: ")
+
+circunferencia = input("Ingrese la ecuación de la circunferencia")
