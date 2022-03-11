6323fb1214ce04de3e0946d838528e3b4ec752b7
AdairNicolas <adairnicolas1@gmail.com>
2022-03-10 20:30:52 -0600

    cambios finales

--- TransformacionDeMobius.py
+++ TransformacionDeMobius.py
@@ -1,5 +1,11 @@
 
 from gettext import find
+from turtle import color
+import matplotlib.pyplot as plt
+
+ec1 =[]
+ec2 =[]
+ec3 =[]
 
 def numcomp(z1):
 
@@ -49,7 +55,23 @@ def division(a,b,a2,b2):
 def valoresc (cir):
     tip = cir[0:1]
     if(tip == "("):
-        return cir
+        hlim = cir.find("-")
+        hfin = cir.find(")")
+        hp = cir[hlim+1:hfin]
+        klim = cir.find("-",hfin)
+        kfin = cir.find(")",klim)
+        kp = cir[klim+1:kfin]
+        rlim = cir.find("=",kfin)
+        rp = cir[rlim+1:]
+        h = float(hp)
+        k = float(kp)
+        rpp = float(rp)
+        r = pow(rpp,0.5)
+        print(h)
+        print(k)
+        print(r)
+
+        return h,k,r
     elif(tip!="("):
         lima = cir.find("(")
         AS = cir[0:lima]
@@ -100,8 +122,18 @@ def mobius(x,y,r1,i1,r2,i2,r3,i3,r4,i4):
     res = division(numeradorr,numeradori,denominadorr,denominadori)
     return res
 
+def sistemadeecuaciones(p1r,p1i,p2r,p2i,p3r,p3i):
+    A1 = (2*p2r)-(2*p1r)
+    B1 = (2*p2i)-(2*p1i)
+    C1 = (pow(p2r,2)+pow(p2i,2))-(pow(p1r,2)+pow(p1i,2))
+    A2 = (2*p3r)-(2*p1r)
+    B2 = (2*p3i)-(2*p1i)
+    C2 = (pow(p3r,2)+pow(p3i,2))-(pow(p1r,2)+pow(p1i,2))
 
-
+    hn = ((C1*B2) - (C2*B1))/((A1*B2)-(A2*B1))
+    kn = ((C1*A2)-(C2*A1))/((B1*A2)-(B2*A1))
+    rn = pow(pow(p1r-hn,2)+ pow(p1i-kn,2),0.5)
+    return hn,kn,rn
 
 print("Bienvenido al programa sobre las transformacxiones de Mobius\n")
 
@@ -117,14 +149,33 @@ r2,i2 = numcomp(z2)
 r3,i3 = numcomp(z3)
 r4,i4 = numcomp(z4)
 
-h,k,r =valoresc(circunferencia)    
+h,k,r =valoresc(circunferencia)
+
+print(f"(x-{h})^2+(y-{k})^2={r}^2")
 
 x1,y1,x2,y2,x3,y3 = obtenerpuntos(h,k,r)
 
 p1 = mobius(x1,y1,r1,i1,r2,i2,r3,i3,r4,i4)
-p2 = mobius(x1,y1,r1,i1,r2,i2,r3,i3,r4,i4)
-p3 = mobius(x1,y1,r1,i1,r2,i2,r3,i3,r4,i4)
+p2 = mobius(x2,y2,r1,i1,r2,i2,r3,i3,r4,i4)
+p3 = mobius(x3,y3,r1,i1,r2,i2,r3,i3,r4,i4)
 p1r,p1i = numcomp(p1)
 p2r,p2i = numcomp(p2)
 p3r,p3i = numcomp(p3)
-    
+
+h2,k2,r2 = sistemadeecuaciones(p1r,p1i,p2r,p2i,p3r,p3i)
+
+print(f"(x-{h2})^2+(y-{k2})^2={r2}^2")
+
+
+circunoriginal = plt.Circle((h,k),r,color='b',fill=False)
+circuntransfor = plt.Circle((h2,k2),r2,color='r',fill=False)
+
+fig,ax = plt.subplots()
+
+ax.set_xlim((-60,40))
+ax.set_ylim((-20,80))
+
+ax.add_patch(circunoriginal)
+ax.add_patch(circuntransfor)
+
+fig.savefig('Circulos.png')
